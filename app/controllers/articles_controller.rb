5be8cc5270029f529c027e181b41d836eb448611
salogod <david.salomon.nava11@gmail.com>
2024-10-29 19:09:42 -0600

    errors display functional

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
   def index
     @Articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 end
