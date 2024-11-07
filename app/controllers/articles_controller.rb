ac2945b8db6c26a7e9c60fa43f6589386972d68b
salogod <david.salomon.nava11@gmail.com>
2024-11-07 10:25:02 -0600

    crud finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,16 @@
 class ArticlesController < ApplicationController
   def show
-    @Article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def index
-    @Articles = Article.all
+    @articles = Article.all
   end
 
   def new
     @article = Article.new
   end
 
-<<<<<<< HEAD
   def edit
     @article = Article.find(params[:id])
   end
@@ -20,27 +19,26 @@ class ArticlesController < ApplicationController
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article created successfully."
-=======
-  def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    if @article.save
->>>>>>> 5915444d24d21c59441e553da45559fc717f22f0
       redirect_to @article
     else
       render :new
     end
   end
-<<<<<<< HEAD
 
   def update
     @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article updated successfully"
+      flash[:notice] = "Article updated successfully."
       redirect_to @article
     else
       render "edit"
     end
   end
-=======
->>>>>>> 5915444d24d21c59441e553da45559fc717f22f0
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article deleted successfully."
+    redirect_to articles_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
-<<<<<<< HEAD
-  resources :articles, only: [ :show, :index, :new, :create, :edit, :update ]
-=======
-  resources :articles, only: [ :show, :index, :new, :create ]
->>>>>>> 5915444d24d21c59441e553da45559fc717f22f0
+
+  resources :articles
 end
